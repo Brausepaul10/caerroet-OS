0c101b6a511bb3fedf337353ed5afa07c96b2e11
Brausepaul10 <jannikfurth1@gmail.com>
2024-04-03 03:27:20 +0200

    fixed everything

--- boot.asm
+++ boot.asm
@@ -1,20 +1,58 @@
 [org 0x7C00]
 
-mov si, msg
-call printString
+jmp main
+; function that reads a key
+readKey:
+    mov ah, 0
+    int 0x16
+    ret
 
+; main part of the operating system
+main:
+    mov si, inputPrompt
+    call printString
+
+; read and display input
+inputloop:
+    call readKey
+    mov ah, al
+    cmp ah, 13
+    je endInputLoop
+    mov [buffer], ah
+    mov si, buffer
+    call printChar
+    jmp inputloop
+
+endInputLoop:
+    jmp $
+
+
+; function that prints a character
+printChar:
+    mov ah, 0x0E
+    int 0x10
+    ret
+
+; function that prints a string
 printString:
     lodsb
     cmp al, 0
-    je end
+    je endPrintString
     mov ah, 0x0E
     int 0x10
     jmp printString
 
-end:
-    jmp $
+endPrintString:
+    ret
 
-msg: db "Hello world!", 0
+; prompt that tells the user to enter input
+inputPrompt:
+    db "Enter a sentence: ", 0
+
+buffer:
+    db 0
+
+
 
 times 510-($-$$) db 0
 dw 0xAA55
